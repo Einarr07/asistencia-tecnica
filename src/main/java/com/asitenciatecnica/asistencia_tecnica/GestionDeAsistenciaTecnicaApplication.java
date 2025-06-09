6c14e0f1c5a57077b7bedc35a7eaf02e72b8bccb
Einarr07 <aidenperse@gmail.com>
2025-06-09 11:43:55 -0500

    Inicialización de la aplicación

---
+++ src/main/java/com/asitenciatecnica/asistencia_tecnica/GestionDeAsistenciaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.asitenciatecnica.asistencia_tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeAsistenciaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeAsistenciaTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asitenciatecnica/asistencia_tecnica/GestionDeAsistenciaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asitenciatecnica.asistencia_tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeAsistenciaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
